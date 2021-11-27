027a52945a00ca7d245b842081f2068cb6ed1b19
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-27 21:19:27 +0800

    Requests

--- src/OneMind/Requests/CreateTeamRequest.cs
+++ src/OneMind/Requests/CreateTeamRequest.cs
@@ -1,8 +1,10 @@
 ﻿namespace OneMind.Requests;
 
-public record CreateOrganizationRequest : IRequest<int>
+public record CreateTeamRequest : IRequest<int>
 {
     public string Name { get; init; } = "";
     public string? Description { get; init; }
     public int LeaderId { get; init; }
+    public short Visibility { get; init; }
+    public int? OrganizationId { get; init; }
 }
