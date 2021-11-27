40771c09de768bb0ca12227f90da673b0845255c
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-27 17:17:21 +0800

    Organization Events

---
+++ src/OneMind/Events/Publishers/OrganizationCreatedEventPublisher.cs
@@ -0,0 +1,23 @@
+﻿namespace OneMind.Events.Publishers;
+
+sealed class OrganizationCreatedEventPublisher : IPipelineBehavior<CreateOrganizationRequest, int>
+{
+    readonly IMediator _mediator;
+
+    public OrganizationCreatedEventPublisher(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<int> Handle(CreateOrganizationRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<int> next)
+    {
+        var id = await next();
+        await PublishAsync(id, cancellationToken);
+        return id;
+    }
+
+    private async Task PublishAsync(int id, CancellationToken cancellationToken)
+    {
+        await _mediator.Publish(new OrganizationCreatedEvent { Id = id }, cancellationToken);
+    }
+}
