a6bb21bf56f4812b11f4ce01d29c394fb771b31d
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-27 17:17:39 +0800

    User exceptions

---
+++ src/OneMind/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace OneMind.Exceptions;
+
+public class UserNotFoundException : CodeCompanionException
+{
+    public int Id { get; init; }
+
+    protected override void SetClientMessage(StringBuilder builder) => builder.Append("User does not exists");
+
+    protected override void SetErrorData(IDictionary<string, object?> errorData) => errorData.FluentAdd(nameof(Id), Id);
+}
