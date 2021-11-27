d6e4c9fcce45f0d806ffab533a0a10a84adab2fe
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-27 21:19:51 +0800

    Handling and Processing

--- src/OneMind.Persistence/Handlers/CreateOrganizationHandler.cs
+++ src/OneMind.Persistence/Handlers/CreateOrganizationHandler.cs
@@ -16,20 +16,30 @@ sealed class CreateOrganizationHandler : IRequestHandler<CreateOrganizationReque
         using var context = await _contextFactory.CreateDbContextAsync(cancellationToken);
         using var transaction = await context.Database.BeginTransactionAsync(cancellationToken);
 
-        var leader =  await context.Users.GetRequiredAsync(request.LeaderId, cancellationToken);
-
-        var id = await _insertOrganization.ExecuteAsync(
-            context.WithHotSave(),
-            new()
-            {
-                Name = request.Name,
-                Description = request.Description,
-                Leader = leader,
-                LeaderId = leader.Id
-            },
-            cancellationToken);
-
+        var leader = await GetLeaderAsync(context, request, cancellationToken);
+        var id = await InsertOrganizationAsync(context, request, leader, cancellationToken);
         await transaction.CommitAsync(cancellationToken);
         return id;
     }
+
+    async Task<int> InsertOrganizationAsync(OneMindDbContext context, CreateOrganizationRequest request, User leader, CancellationToken cancellationToken) => await _insertOrganization.ExecuteAsync(
+        context.WithHotSave(),
+        new()
+        {
+            Name = request.Name,
+            Description = request.Description,
+            Leader = leader,
+            LeaderId = leader.Id
+        },
+        cancellationToken
+    );
+
+    static async Task<User> GetLeaderAsync(OneMindDbContext context, CreateOrganizationRequest request, CancellationToken cancellationToken) => await context.Users.GetAsync(request.LeaderId, cancellationToken)
+        ?? throw new OrganizationLeaderRequiredException
+        {
+            Organization = new()
+            {
+                Name = request.Name
+            }
+        };
 }
---
+++ src/OneMind.Persistence/Processes/InsertTeam.cs
@@ -0,0 +1,14 @@
+﻿namespace OneMind.Processes;
+
+sealed class InsertTeam : IAsyncProcess<Team, int>
+{
+    public async Task<int> ExecuteAsync(IProcessContext processContext, Team team, CancellationToken cancellationToken = default)
+    {
+        if (processContext is not OneMindDbContext context)
+            throw InvalidProcessContextException.Expects<OneMindDbContext>();
+
+        context.Teams.Add(team, context.CurrentFootprint);
+        await context.TrySaveChangesAsync(cancellationToken);
+        return team.Id;
+    }
+}
