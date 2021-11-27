201d55ea9a90eea0dbb8b26acac387e821426bec
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-27 17:17:47 +0800

    Requests

---
+++ src/OneMind/Requests/CreateOrganizationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace OneMind.Requests;
+
+public record CreateOrganizationRequest : IRequest<int>
+{
+    public string Name { get; init; } = "";
+    public string? Description { get; init; }
+    public int LeaderId { get; init; }
+}
