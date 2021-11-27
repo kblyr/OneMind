6857baaf628a08a88748c20cb82ae1c049699a28
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-27 17:18:15 +0800

    Handling and Processes

---
+++ src/OneMind.Persistence/Handlers/CreateOrganizationHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace OneMind.Handlers;
+
+sealed class CreateOrganizationHandler : IRequestHandler<CreateOrganizationRequest, int>
+{
+    readonly IDbContextFactory<OneMindDbContext> _contextFactory;
+    readonly InsertOrganization _insertOrganization;
+
+    public CreateOrganizationHandler(IDbContextFactory<OneMindDbContext> contextFactory, InsertOrganization insertOrganization)
+    {
+        _contextFactory = contextFactory;
+        _insertOrganization = insertOrganization;
+    }
+
+    public async Task<int> Handle(CreateOrganizationRequest request, CancellationToken cancellationToken)
+    {
+        using var context = await _contextFactory.CreateDbContextAsync(cancellationToken);
+        using var transaction = await context.Database.BeginTransactionAsync(cancellationToken);
+
+        var leader =  await context.Users.GetRequiredAsync(request.LeaderId, cancellationToken);
+
+        var id = await _insertOrganization.ExecuteAsync(
+            context.WithHotSave(),
+            new()
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Leader = leader,
+                LeaderId = leader.Id
+            },
+            cancellationToken);
+
+        await transaction.CommitAsync(cancellationToken);
+        return id;
+    }
+}
---
+++ src/OneMind.Persistence/Processes/InsertOrganization.cs
@@ -0,0 +1,14 @@
+﻿namespace OneMind.Processes;
+
+sealed class InsertOrganization : IAsyncProcess<Organization, int>
+{
+    public async Task<int> ExecuteAsync(IProcessContext processContext, Organization organization, CancellationToken cancellationToken = default)
+    {
+        if (processContext is not OneMindDbContext context)
+            throw InvalidProcessContextException.Expects<OneMindDbContext>();
+
+        context.Organizations.Add(organization, context.CurrentFootprint);
+        await context.TrySaveChangesAsync(cancellationToken);
+        return organization.Id;
+    }
+}
